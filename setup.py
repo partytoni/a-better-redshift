7f7bc1ffe5614c9163aa98c1093f734d31e95148
Antonio Martino <ant.mar94@gmail.com>
2019-02-20 16:47:32 +0100

    setup.py is working

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 import os
 
-os.system("sudo apt-get install redshift")
-
-#CURRENTLY NOT WORKING
+print("\n\nTo use Bettershift, you need redshift and libappindicator3.\n\n")
 
 os.system("touch "+os.path.abspath(os.path.dirname(__file__))+"/abetterredshift.desktop")
 os.system("chmod 777 "+os.path.abspath(os.path.dirname(__file__))+"/abetterredshift.desktop")
 file_name = os.path.abspath(os.path.dirname(__file__)) + "/abetterredshift.desktop"
 with open(file_name, 'w') as f:
-    try:
-        f.write("Name=A Better Redshift\n")
-        f.write("Exec="+os.path.abspath(os.path.dirname(__file__))+"/a-better-redshift.py\n")
-        f.write("Icon="+os.path.abspath(os.path.dirname(__file__))+"/icon.png\n")
-        f.close()
-    except:  # whatever reader errors you care about
-        print("ERROR\n\n")
-        # setting temperature
+	try:
+		f.write("[Desktop Entry]\n")
+		f.write("Name=A Better Redshift\n")
+		f.write("Terminal=false\n")
+		f.write("Type=Application\n")
+		f.write("Exec="+os.path.abspath(os.path.dirname(__file__))+"/a-better-redshift.py\n")
+		f.write("Icon="+os.path.abspath(os.path.dirname(__file__))+"/big_icon.png\n")
+		f.close()
+	except:  
+		print("ERROR\n\n")
+		
 os.system("mv "+os.path.abspath(os.path.dirname(__file__))+"/abetterredshift.desktop ~/.local/share/applications")
