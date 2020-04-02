c64731ce543fc07c804debec7709195033dee839
Antonio Martino <ant.mar94@gmail.com>
2020-04-02 12:01:23 +0200

    fixed error in setup.py where the folder applications was not created before and the launcher was called applications

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 import os
 
-print("\n\nTo use Bettershift, you need redshift and libappindicator3.\n\n")
+print("\n\nTo use Bettershift, you need redshift and libappindicator3.\n\nsudo apt install redshift libappindicator3\n\n")
 
-os.system("touch "+os.path.abspath(os.path.dirname(__file__))+"/abetterredshift.desktop")
-os.system("chmod 777 "+os.path.abspath(os.path.dirname(__file__))+"/abetterredshift.desktop")
-file_name = os.path.abspath(os.path.dirname(__file__)) + "/abetterredshift.desktop"
+dir = os.path.abspath(os.path.dirname(__file__))
+
+os.system("touch "+dir+"/abetterredshift.desktop")
+os.system("chmod 777 "+dir+"/abetterredshift.desktop")
+file_name = dir + "/abetterredshift.desktop"
 with open(file_name, 'w') as f:
 	try:
 		f.write("[Desktop Entry]\n")
 		f.write("Name=A Better Redshift\n")
 		f.write("Terminal=false\n")
 		f.write("Type=Application\n")
-		f.write("Exec="+os.path.abspath(os.path.dirname(__file__))+"/a-better-redshift.py\n")
-		f.write("Icon="+os.path.abspath(os.path.dirname(__file__))+"/icon.png\n")
+		f.write("Exec="+dir+"/a-better-redshift.py\n")
+		f.write("Icon="+dir+"/icon.png\n")
 		f.close()
-		os.system("mv "+os.path.abspath(os.path.dirname(__file__))+"/abetterredshift.desktop ~/.local/share/applications")
+		os.system("mkdir -p ~/.local/share/applications")
+		os.system("mv "+dir+"/abetterredshift.desktop ~/.local/share/applications")
 		print ("A menu entry has been added to ~/.local/share/applications")
 	except:  
 		print("ERROR\n\n")
